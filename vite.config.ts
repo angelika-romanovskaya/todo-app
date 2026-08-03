ddad2baa0307850f96390aaa32d0b23165ede42b
Angelika Romanovskaya <romanovskaya.a@ifuture.by>
2026-08-03 21:38:56 +0300

    add env

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-	base: "/todo-app/",
+	base: process.env.BASE_URL || "/",
 	plugins: [react()],
 	resolve: {
 		alias: {
